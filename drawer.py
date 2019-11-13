d715d301c8c7df03f13604666491fb10ebe5e13c
Jacob Denbeaux <denbeaux@mit.edu>
2019-11-12 21:30:55 -0500

    moved functionality from drawer to driver for more consistency

--- drawer.py
+++ drawer.py
@@ -1,135 +1,8 @@
-from driver import AxiCLI
+from driver import AxiCLI, Path, Point
 import time, math, random
 
 # no need to generalize for n dimensions, because axidraw is 2d
 # maybe later if we decide to use something like paintbrushes, we can add a third dimension
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __round__(self):
-        return Point(round(self.x), round(self.y))
-
-    def __add__(self, other):
-        if isinstance(other, int) or isinstance(other, float):
-            return Point(self.x + other, self.y + other)
-        return Point(self.x + other.x, self.y + other.y)
-
-    def __sub__(self, other):
-        return self + -other
-
-    def __rsub__(self, other):
-        return -self + other
-
-    def __mul__(self, other):
-        if isinstance(other, int) or isinstance(other, float):
-            return Point(self.x * other, self.y * other)
-        return Point(self.x * other.x, self.y * other.y)
-
-    def __truediv__(self, other):
-        if isinstance(other, int) or isinstance(other, float):
-            return Point(self.x / other, self.y / other)
-        return Point(self.x / other.x, self.y / other.y)
-
-    __rmul__ = __mul__
-    __radd__ = __add__
-
-    def __rsub__(self, other):
-        return -(self - other)
-
-    def __rtruediv__(self, other):
-        if isinstance(other, int) or isinstance(other, float):
-            return Point(other / self.x, other / self.y)
-        return Point(other.x / self.x, other.y / self.y)
-
-    def __neg__(self):
-        return -1 * self
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __list__(self):
-        return [self.x, self.y]
-
-    def __tuple(self):
-        return (self.x, self.y)
-
-    def __str__(self):
-        return '({}, {})'.format(self.x, self.y)
-
-    def __iter__(self):
-        return iter([self.x, self.y])
-
-class Path:
-    def __init__(self, points):
-        self.path = points
-        if self.path == []:
-            raise ValueError('Empty Path!')
-        if not all(isinstance(i, Point) for i in self.path):
-            raise ValueError('Non Point Types in Path!')
-
-    def __mul__(self, scalar):
-        return Path([scalar * point for point in self.path])
-
-    __rmul__ = __mul__
-
-    def __truediv__(self, scalar):
-        return Path([point / scalar for point in self.path])
-
-    def __add__(self, other):
-        # we want to shift our path
-        if isinstance(other, int) or isinstance(other, float):
-            return Path([other + point for point in self.path])
-
-        # otherwise, we want to join two paths
-        return Path(self.path + other.path)
-
-    def __radd__(self, other):
-        if isinstance(other, int) or isinstance(other, float):
-            return Path([other + point for point in self.path])
-        return Path(other.path + self.path)
-
-    def __str__(self):
-        # return '\n'.join([str(i) for i in self.path])
-        return '\n'.join(map(str, self.path))
-
-    def __round__(self):
-        return Path([round(point) for point in self.path])
-
-    def snap_to_grid(self, grid_size=8):
-        self.path = grid_size * round(self / grid_size)
-        # return Path(grid_size * round(self / grid_size))
-
-    def __getitem__(self, key):
-        return self.path[key]
-
-    # remove duplicate elements in path
-    def prune(self):
-        # start with first element
-        # only add new elements if they differ from the most recent one
-        new_path = [self.path[0]]
-        for point in self.path[1:]:
-            if point != new_path[-1]:
-                new_path.append(Point(*point))
-        self.path = new_path
-
-    def shuffle(self, closed=True):
-        random.shuffle(self.path)
-        if closed:
-            print(self.path[0])
-            self.path.append(self.path[0])
-
-    def draw(self, axi):
-        self.snap_to_grid()
-        self.prune()
-        # self.path = self.prune(self.snap_to_grid())
-        axi.pen_up()
-        axi.move_to(tuple([*self.path[0]]))
-        axi.pen_down()
-        for point in self.path[1:]:
-            axi.move_to(tuple([*point]))
-
 def circle(n=None):
     if n is None:
         n = 10000
@@ -148,8 +21,8 @@ def random_filled_circle_paths(radius):
     inner.shuffle()
     return outer, inner
 
-paths = random_filled_circle_paths(1000)
+paths = random_filled_circle_paths(400)
 axi = AxiCLI()
 for path in paths:
-    path.draw(axi)
+    axi.draw(path)
 axi.close()
--- driver.py
+++ driver.py
@@ -1,21 +1,15 @@
 import serial, serial.tools.list_ports
-import time
+import time, random, math
 
 # set something up so that deleting or dereferencing the object forces close
 # we want this to really be the "with open() as f" type structure
-
-def point_sum(a, b):
-    return (a[0] + b[0], a[1] + b[1])
-
-def point_diff(a, b):
-    return point_sum(a, (-b[0], -b[1]))
 
 class AxiCLI:
     def __init__(self, microstepping=1):
         self.port_connector = self.fetch_port()
         self.axi = None
         self.open()
-        self.pos = (0, 0)
+        self.pos = Point(0, 0)
 
     # if axidraw is connected, this returns the port to connect to
     # if multiple axidraws are connected, this picks the first one - sorry!
@@ -40,7 +34,7 @@ class AxiCLI:
             raise RuntimeException('AxiDraw connection is already closed!')
         time.sleep(0.1)
         self.pen_up()
-        self.move_to((0, 0))
+        self.move_to(Point(0, 0))
         self.command('EM', 0, 0) # turn off motors
         self.axi.close()
 
@@ -65,8 +59,136 @@ class AxiCLI:
         duration = max(map(abs, move)) // STEPS_PER_MS
         self.command('XM', duration, *move)
         time.sleep(duration / 1000)
-        self.pos = point_sum(self.pos, move)
+        self.pos += move
+        # self.pos = point_sum(self.pos, move)
 
     def move_to(self, pos):
-        move = point_diff(pos, self.pos)
+        # move = point_diff(pos, self.pos)
+        move = pos - self.pos
         self.line(move)
+
+    def draw(self, path):
+        path.snap_to_grid()
+        path.prune()
+        self.pen_up()
+        self.move_to(path[0])
+        self.pen_down()
+        for point in path[1:]:
+            self.move_to(point)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __round__(self):
+        return Point(round(self.x), round(self.y))
+
+    def __add__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Point(self.x + other, self.y + other)
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return self + -other
+
+    def __rsub__(self, other):
+        return -self + other
+
+    def __mul__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Point(self.x * other, self.y * other)
+        return Point(self.x * other.x, self.y * other.y)
+
+    def __truediv__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Point(self.x / other, self.y / other)
+        return Point(self.x / other.x, self.y / other.y)
+
+    __rmul__ = __mul__
+    __radd__ = __add__
+
+    def __rsub__(self, other):
+        return -(self - other)
+
+    def __rtruediv__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Point(other / self.x, other / self.y)
+        return Point(other.x / self.x, other.y / self.y)
+
+    def __neg__(self):
+        return -1 * self
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __list__(self):
+        return [self.x, self.y]
+
+    def __tuple(self):
+        return (self.x, self.y)
+
+    def __str__(self):
+        return '({}, {})'.format(self.x, self.y)
+
+    def __iter__(self):
+        return iter([self.x, self.y])
+
+class Path:
+    def __init__(self, points):
+        self.path = points
+        if self.path == []:
+            raise ValueError('Empty Path!')
+        if not all(isinstance(i, Point) for i in self.path):
+            raise ValueError('Non Point Types in Path!')
+
+    def __mul__(self, scalar):
+        return Path([scalar * point for point in self.path])
+
+    __rmul__ = __mul__
+
+    def __truediv__(self, scalar):
+        return Path([point / scalar for point in self.path])
+
+    def __add__(self, other):
+        # we want to shift our path
+        if isinstance(other, int) or isinstance(other, float) or isinstance(other, Point):
+            return Path([other + point for point in self.path])
+
+        # otherwise, we want to join two paths
+        return Path(self.path + other.path)
+
+    def __radd__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            return Path([other + point for point in self.path])
+        return Path(other.path + self.path)
+
+    def __str__(self):
+        # return '\n'.join([str(i) for i in self.path])
+        return '\n'.join(map(str, self.path))
+
+    def __round__(self):
+        return Path([round(point) for point in self.path])
+
+    def snap_to_grid(self, grid_size=8):
+        self.path = grid_size * round(self / grid_size)
+        # return Path(grid_size * round(self / grid_size))
+
+    def __getitem__(self, key):
+        return self.path[key]
+
+    # remove duplicate elements in path
+    def prune(self):
+        # start with first element
+        # only add new elements if they differ from the most recent one
+        new_path = [self.path[0]]
+        for point in self.path[1:]:
+            if point != new_path[-1]:
+                new_path.append(Point(*point))
+        self.path = new_path
+
+    def shuffle(self, closed=True):
+        random.shuffle(self.path)
+        if closed:
+            self.path.append(self.path[0])
